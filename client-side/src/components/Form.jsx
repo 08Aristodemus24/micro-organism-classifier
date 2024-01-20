9d51692866972675bc5cd988d1914023fd68c1e0
08Miguel24 <MichaelAveuc571@gmail.com>
2024-01-20 14:39:10 +0800

    update

---
+++ client-side/src/components/Form.jsx
@@ -0,0 +1,106 @@
+import ImageInput from './ImageInput';
+import Button from "./Button";
+
+import { useContext, useState } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { DesignsContext } from "../contexts/DesignsContext";
+import { FormInputsContext } from "../contexts/FormInputsContext";
+
+
+export default function Form(){
+    let [image, setImage] = useState(null);
+
+    let style;
+    const designs = useContext(DesignsContext);
+    const themes = useContext(ThemeContext);
+    const { design, theme } = themes;
+    
+    // sometimes themes context will contain only the design 
+    // and not the theme key so check if theme key is in themes
+    if('theme' in themes){
+        style = designs[design][theme];
+    }else{
+        style = designs[design];
+    }
+
+    // send a post request and retrieve the response and set 
+    // the state of the following states for the alert component
+    let [response, setResponse] = useState(null);
+    let [msgStatus, setMsgStatus] = useState();
+    let [errorType, setErrorType] = useState(null);
+    const handleSubmit = async (event) => {
+        try {
+            event.preventDefault();
+            const form_data = new FormData();
+            form_data.append('image', image);
+
+            // once data is validated submitted and then extracted
+            // reset form components form element
+            setImage(null);
+
+            // send here the data from the contact component to 
+            // the backend proxy server
+            // // for development
+            const url = 'http://127.0.0.1:5000/predict';
+            // for production
+            // const url = 'https://project-alexander.vercel.app/predict';
+
+            const resp = await fetch(url, {
+                'method': 'POST',
+                'body': form_data,
+            });
+            setResponse(resp);
+
+            // if response.status is 200 then that means contact information
+            // has been successfully sent to the email.js api
+            if(resp.status === 200){
+                setMsgStatus("success");
+                console.log(`message has been sent with code ${resp.status}`);
+
+            }else{
+                setMsgStatus("failure");
+                console.log(`message submission unsucessful. Response status '${resp.status}' occured`);
+            }
+
+        }catch(error){
+            setMsgStatus("denied");
+            setErrorType(error);
+            console.log(`Submission denied. Error '${error}' occured`);
+        }
+    };
+
+    console.log(`response: ${response}`);
+    console.log(`message status: ${msgStatus}`);
+    console.log(`error type: ${errorType}`)
+
+    return (
+        <FormInputsContext.Provider value={{
+            image, setImage,
+            handleSubmit,
+        }}>
+            <div className="form-container">
+                <form
+                    className={`form ${design}`}
+                    style={style}
+                    method="POST"
+                >
+                    <ImageInput/>
+                    <Button/>
+                </form>
+                <div className={`alert ${msgStatus !== undefined ? 'show' : ''}`} onClick={(event) => {
+                    // remove class from alert container to hide it again
+                    event.target.classList.remove('show');
+                
+                    // reset msg_status to undefined in case of another submission
+                    setMsgStatus(undefined);
+                }}>
+                    <div className="alert-wrapper">
+                        {msgStatus === "success" || msgStatus === "failed" ? 
+                        <span className="alert-message">Message has been sent with code {response?.status}</span> : 
+                        <span className="alert-message">Submission denied. Error {errorType?.message} occured</span>}
+                    </div>
+                </div>
+            </div>
+        </FormInputsContext.Provider>
+    );
+}
---
+++ server-side/server.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+
+from datetime import datetime as dt
+
+# ff. imports are for getting secret values from .env file
+from pathlib import Path
+import os
+
+from modelling.utilities.preprocessors import (
+    translate_labels,
+    encode_image,
+    standardize_image,
+    re_encode_sparse_labels,
+    decode_one_hot,
+    activate_logits
+)
+from modelling.utilities.visualizers import (
+    show_image
+)
+
+import tensorflow as tf
+import numpy as np
+
+# configure location of build file and the static html template file
+app = Flask(__name__, template_folder='static')
+
+# since simple html from url http://127.0.0.1:5000 requests to
+# api endpoint at http://127.0.0.1:5000/ we must set the allowed
+# origins or web apps with specific urls like http://127.0.0.1:5000
+# to be included otherwise it will be blocked by CORS policy
+CORS(app, origins=["http://localhost:5173", "http://127.0.0.1:5000",])
+
+models = []
+
+def load_models():
+    """
+    prepares and loads sample input and custom model in
+    order to use trained weights/parameters/coefficients
+    """
+
+    # recreate model architecture
+    saved_model = tf.keras.models.load_model('./modelling/final/models/test_architecture-inception-v3_10_0.66.h5')
+    models.append(saved_model)
+
+load_models()
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # extract raw data from client
+    raw_files = request.files
+    print(raw_files)
+
+    image = raw_files.get('image')
+
+    # preprocessing/encoding image stream into a matrix
+    encoded_img = encode_image(image.stream)
+    rescaled_img = standardize_image(encoded_img)
+    
+    # reshape the image since the model takes in an (m, 256, 256, 3)
+    # input, or in this case a single (1, 256, 256, 3) input
+    img_shape = rescaled_img.shape
+    reshaped_img = np.reshape(rescaled_img, newshape=(1, img_shape[0], img_shape[1], img_shape[2]))
+    
+    # predictor
+    logits = models[0].predict(reshaped_img)
+
+    # decoding stage
+    Y_preds = activate_logits(logits)
+    Y_preds = decode_one_hot(Y_preds)
+    final_preds = re_encode_sparse_labels(Y_preds, new_labels=['Amoeba', 'Euglena', 'Hydra', 'Paramecium', 'Rod_bacteria', 'Spherical_bacteria', 'Spiral_bacteria', 'Yeast'])
+    print(final_preds)
+    
+    return jsonify({'prediction': final_preds})
